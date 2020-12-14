cc61577395686915080a32f9c5efa7c4e4635214
annieyxu <annieyxu@berkeley.edu>
2020-12-14 15:43:59 -0800

    phase 1 done

--- W5D1/hash_map/lib/p01_int_set.rb
+++ W5D1/hash_map/lib/p01_int_set.rb
@@ -51,7 +51,6 @@ class IntSet
   end
 
   def include?(num)
-    # debugger
     self[num].include?(num)
   end
 
@@ -69,7 +68,8 @@ class IntSet
 end
 
 class ResizingIntSet
-  attr_reader :count
+  attr_accessor :store, :count
+
 
   def initialize(num_buckets = 20)
     @store = Array.new(num_buckets) { Array.new }
@@ -77,24 +77,39 @@ class ResizingIntSet
   end
 
   def insert(num)
+    if !include?(num)
+      self[num] << num 
+      @count += 1
+    end
+    resize! if num_buckets < self.count
   end
 
   def remove(num)
+    if include?(num)
+      self[num].delete(num)
+      @count -= 1
+    end
   end
 
   def include?(num)
+    self[num].include?(num)
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
   end
 
   def num_buckets
-    @store.length
+    self.store.length
   end
 
   def resize!
+    old_store = self.store
+    @count = 0
+    self.store = Array.new(num_buckets * 2) {Array.new}
+    old_store.flatten.each {|ele| insert(ele)}
   end
 end

b9050a924f22bd9a3720a2b2a5a6d9c2dbce7b31
michelle ha <michelle1992us@yahoo.com>
2020-12-14 16:42:08 -0800

    finished part 2

--- W5D1/hash_map/lib/p02_hashing.rb
+++ W5D1/hash_map/lib/p02_hashing.rb
@@ -4,11 +4,24 @@ end
 
 class Array
   def hash
+    sum = 0
+    self.each_with_index do |ele, i|
+      sum += ele.hash * i
+    end
+    sum
   end
 end
 
+
+
 class String
   def hash
+    sum = 0
+    self.each_char.with_index do |char, i|
+      sum += char.ord.hash * i 
+      #.ord coverts a character of a string into a value between 0-225
+    end
+    sum
   end
 end
 
@@ -16,6 +29,6 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    self.keys.sort.hash
   end
 end

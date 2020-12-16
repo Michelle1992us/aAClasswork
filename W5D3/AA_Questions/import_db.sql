e82c7c113a1dff032f9caf41ee7232472f1fccce
Trevor Smith <trevor.smith1667@gmail.com>
2020-12-16 14:04:54 -0800

    added tables for users, questions, and qfollows

---
+++ W5D3/AA_Questions/import_db.sql
@@ -0,0 +1,26 @@
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY, 
+    fname TEXT NOT NULL, 
+    lname TEXT NOT NULL
+
+);
+
+CREATE TABLE questions (
+    id INTEGER PRIMARY KEY, 
+    title TEXT NOT NULL, 
+    body TEXT NOT NULL
+    author_id INTEGER NOT NULL 
+
+    FOREIGN KEY(author_id) REFERENCES users(id)
+);
+
+CREATE TABLE question_follows (
+    id INTEGER PRIMARY KEY, 
+    users_id INTEGER NOT NULL, 
+    questions_id INTEGER NOT NULL, 
+    
+);
+
+CREATE TABLE replies (
+    
+)

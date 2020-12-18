4d7bafc1ce09796f651cf27e284a8296c9450f51
Itai Farhi <itaimf@gmail.com>
2020-12-18 14:17:42 -0800

    Complete director exercise

--- W5D5/movie_buff/movie_buff/01_queries.rb
+++ W5D5/movie_buff/movie_buff/01_queries.rb
@@ -38,6 +38,12 @@ def biggest_cast
   # Find the id and title of the 3 movies with the
   # largest casts (i.e most actors)
 
+  Movie.select(:id, :title)
+       .joins(:actors)
+       .group('movies.id')
+       .order('COUNT(actors.id) DESC')
+       .limit(3)
+
 end
 
 def directed_by_one_of(them)
@@ -52,6 +58,8 @@ def directed_by_one_of(them)
   # Movie.where(yr: years)
   #
   # Find the id and title of all the movies directed by one of 'them'.
+
+  Movie.select(:id, :title).joins(:director).where(actors: {name: them})
 
 end
 

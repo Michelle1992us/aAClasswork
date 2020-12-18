249c64820af8a25006d98feb3e8421cfdc52efa4
Itai Farhi <itaimf@gmail.com>
2020-12-18 15:20:17 -0800

    finish queries 2

--- W5D5/movie_buff/movie_buff/02_queries.rb
+++ W5D5/movie_buff/movie_buff/02_queries.rb
@@ -8,20 +8,23 @@ end
 
 def bad_years
   # List the years in which a movie with a rating above 8 was not released.
-  Movie.select(:yr).where("score <= 8")
+  Movie.group(:yr).having("MAX(score) <= 8").pluck(:yr)
 
 end
 
 def cast_list(title)
   # List all the actors for a particular movie, given the title.
   # Sort the results by starring order (ord). Show the actor id and name.
-
+  Actor.select(:id, :name).joins(:movies).where(movies: {title: title}).order("ord")
 end
 
 def vanity_projects
   # List the title of all movies in which the director also appeared
   # as the starring actor.
   # Show the movie id and title and director's name.
+
+  Movie.select(:id, :title, :name).joins(:actors).where("actor_id = director_id")
+       .where("ord = 1")
 
   # Note: Directors appear in the 'actors' table.
 
@@ -30,5 +33,10 @@ end
 def most_supportive
   # Find the two actors with the largest number of non-starring roles.
   # Show each actor's id, name and number of supporting roles.
-
+  Actor.select(:id, :name, "count(*) as roles")
+       .joins(:castings)
+       .where.not("ord = 1")
+       .group("actors.id")
+       .order("roles DESC")
+       .limit(2)
 end

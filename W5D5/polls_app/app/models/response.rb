08792ce6118ce0d5989c9922b6f9175b981eab72
Michelle Ha <michelle1992us@yahoo.com>
2020-12-18 17:00:45 -0800

    finished associations

--- W5D5/polls_app/app/models/response.rb
+++ W5D5/polls_app/app/models/response.rb
@@ -1,2 +1,13 @@
 class Response < ApplicationRecord
+
+    belongs_to :respondent,
+        primary_key: :id,
+        foreign_key: :user_id,
+        class_name: :Response
+
+    belongs_to :answer_choice,
+        primary_key: :id,
+        foreign_key: :answer_choice_id,
+        class_name: :AnswerChoice
+
 end

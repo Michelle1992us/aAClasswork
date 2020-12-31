df9714fd37b72f54b220a7b22597cb1ebda4aa22
Michelle Ha <michelle1992us@yahoo.com>
2020-12-31 15:09:19 -0800

    created index, new, show methods

--- W6D5/cats/app/controllers/cats_controller.rb
+++ W6D5/cats/app/controllers/cats_controller.rb
@@ -10,6 +10,30 @@ class CatsController < ApplicationController
 #          PUT    /cats/:id(.:format)                                                                      cats#update
 #          DELETE /cats/:id(.:format)                                                                      cats#destroy
 
+    def index
+        @cats = Cat.all
+        render :index
+    end
 
+    def show
+        @cat = Cat.find_by(params[:id])
+        render :show
+    end
+
+    def create
+        @cat = Cat.new(cat_params)
+        if @cat
+            render :new
+        else
+            render json: cat.error.full_messages, status: 422
+        end
+    end
+
+
+    private
+
+    def cat_params
+        params.require(:cat).permit(:birth_date, :color, :name, :sex, :description)
+    end
 
 end

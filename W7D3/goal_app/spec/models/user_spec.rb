81514bb4ab7adcd36f191e0e2ad91c321d126a66
Michelle Ha <michelle1992us@yahoo.com>
2021-01-06 16:37:11 -0800

    finished making user model methods for goal_all project

--- W7D3/goal_app/spec/models/user_spec.rb
+++ W7D3/goal_app/spec/models/user_spec.rb
@@ -1,13 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  subject(:user) {FactoryBot.create(:user)}
+  # subject(:user) {FactoryBot.create(:user)}
+  subject(:user) {User.create(username: 'Zac', password: 'password')}
 
   
   it {should validate_presence_of(:username)}
   it {should validate_presence_of(:password_digest)}
   it {should validate_presence_of(:session_token)}
-  # it {should validate_length_of(:password).is_at_least(6)}
+  it {should validate_length_of(:password).is_at_least(6)}
 
 
   it {should validate_uniqueness_of(:username)}
@@ -26,21 +27,18 @@ RSpec.describe User, type: :model do
     it {should have_many(:goals)}
   end
 
-  describe '#password_encryption' do
-    it 'does not save password to the database' do
-      FactoryBot.create(:user, username: 'Diglet')
-      user = User.find_by(username: 'Diglet')
-      expect(user.password).not_to eq('password')
-    end
+  # describe '#password_encryption' do
+  #   it 'does not save password to the database' do
+  #     FactoryBot.create(:user, username: 'Diglet')
+  #     user = User.find_by(username: 'Diglet')
+  #     expect(user.password).not_to eq('password')
+  #   end
 
-    it 'ensure password is encrypted using Bcrypt' do
-      expect(Bcrypt::Password).to receive(:create).with('123456')
-      FactoryBot.build(:user, password: '123456')
-    end
-  end
-
-  #Methods still needed: self.find_by_credentials, is_password?(password), password=(password), reset_session_token!, ensure_session_token
-
+  #   it 'ensure password is encrypted using Bcrypt' do
+  #     expect(Bcrypt::Password).to receive(:create).with('123456')
+  #     FactoryBot.build(:user, password: '123456')
+  #   end
+  # end
 
 
 

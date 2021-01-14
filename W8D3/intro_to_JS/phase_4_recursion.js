95d20203a7dbdc7c15841266946b305a5dce43d7
Michelle Ha <michelle1992us@yahoo.com>
2021-01-13 17:00:42 -0800

    finsihed up to deepDup

--- W8D3/intro_to_JS/phase_4_recursion.js
+++ W8D3/intro_to_JS/phase_4_recursion.js
@@ -14,11 +14,20 @@ function range(start, end) {
     return arr;
 }
 
-console.log(range(2, 5))
+// console.log(range(2, 5))
 
 // sumRec(arr) - receives an array of numbers and recursively sums them
 
+function sumRec(arr) {
+    if (arr.length === 0) {
+        return 0
+    } 
+    // return arr[0] + sumRec(arr.slice(1, arr.length)); 
+    return arr.pop() + sumRec(arr);
 
+
+}
+// console.log(sumRec([1, 5, 7, 10]))
 
 
 
@@ -30,17 +39,65 @@ console.log(range(2, 5))
 // exp(b, 0) = 1
 // exp(b, n) = b * exp(b, n - 1)
 
+function exponent(base, exp) {
+    if (exp === 0) {
+        return 1;
+    } 
+    // if (exp === 1) {
+    //     return base;
+    // } 
+    return base*exponent(base, exp - 1);
+}
+
+// console.log(exponent(2, 5)) //=> 32
+
 // # recursion 2
 // exp(b, 0) = 1
 // exp(b, 1) = b
 // exp(b, n) = exp(b, n / 2) ** 2             [for even n]
 // exp(b, n) = b * (exp(b, (n - 1) / 2) ** 2) [for odd n]
 
+function exp(b, n) {
+    if (n === 0) {
+        return 1;
+    } 
+    if (n === 1) {
+        return b;
+    } 
+    if (n % 2 === 0) {
+        return Math.pow(exp(b, n / 2), 2); 
+    } else {
+        return b*((exp(b, (n - 1) / 2) * (exp(b, (n - 1) / 2))));
+    }
+}
+
+// console.log(exp(2, 4)) //=> 32
+// console.log(Math.pow(2, 5))
 
 // fibonacci(n) - receives an integer, n, and returns the first n Fibonacci numbers
+//[1, 1, 2, 3, 5, ...]
+
+function fibonacci(n) {
+    if (n === 1) {
+        return [1];
+    }
+
+    if (n === 2) {
+        return [1, 1];
+    }
+
+    let arr = fibonacci(n-1) //[1, 1, 2]
+    arr.push(arr[n-2] + arr[n-3]) //[2] + [1] => [3] => [1, 1, 2, 3]
+    // let arr = arr.concat(fibonacci(n-1) + fibonacci(n-2));
+    return arr
+    // return arr.push(fibonacci(n-1) + fibonacci(n-2))  
+}
+
+console.log(fibonacci(4))
 
 
 
 
 
 // deepDup(arr) - deep dup of an Array!
+//[1, 2, 3] 

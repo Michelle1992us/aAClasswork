a37645e91d7d55985e0348e42661a5a4dd199da5
Qi Zhu <rubybfm1017@gmail.com>
2021-01-19 14:09:47 -0800

    curriedSum finished

--- W9D1/arguments_exercises.js
+++ W9D1/arguments_exercises.js
@@ -57,53 +57,60 @@ Function.prototype.myBind = function (ctx, ...bindArgs) { //first arg is ctx, al
 
 //1. Solve it first using the arguments keyword. Within your myBind method, you'll have to define a new, anonymous function to be returned. There are two arrays of arguments you care about: the extra arguments passed to myBind, and the arguments passed when the bound function is called. DO NOT USE ES6 (fat arrow)
 
-Function.prototype.myBind = function (ctx) {
-    let bindArgs = [].slice.call(arguments, 1) //use .slice to create an array. Slicing 1 = first argument
-    let that = this //create variable for "this" b/c no longer using fat arrow, so "this" = "windows" if called in the function
-    return function() { //get rid of fat arrow
-        let callArgs = [].slice.call(arguments) //grab all of the arguments that came in
-        return that.apply(ctx, bindArgs.concat(callArgs)); 
-    }
-}
+// Function.prototype.myBind = function (ctx) {
+//     let bindArgs = [].slice.call(arguments, 1) //use .slice to create an array. Slicing 1 = first argument
+//     let that = this //create variable for "this" b/c no longer using fat arrow, so "this" = "windows" if called in the function
+//     return function() { //get rid of fat arrow
+//         let callArgs = [].slice.call(arguments) //grab all of the arguments that came in
+//         return that.apply(ctx, bindArgs.concat(callArgs)); 
+//     }
+// }
 
 
-class Cat {
-    constructor(name) {
-      this.name = name;
-    }
+// class Cat {
+//     constructor(name) {
+//       this.name = name;
+//     }
   
-    says(sound, person) {
-      console.log(`${this.name} says ${sound} to ${person}!`);
-      return true;
-    }
-  }
+//     says(sound, person) {
+//       console.log(`${this.name} says ${sound} to ${person}!`);
+//       return true;
+//     }
+//   }
   
-  class Dog {
-    constructor(name) {
-      this.name = name;
-    }
-  }
+//   class Dog {
+//     constructor(name) {
+//       this.name = name;
+//     }
+//   }
   
-  const markov = new Cat("Markov");
-  const pavlov = new Dog("Pavlov");
+//   const markov = new Cat("Markov");
+//   const pavlov = new Dog("Pavlov");
   
-  markov.says("meow", "Ned"); // Markov says meow to Ned!
+//   markov.says("meow", "Ned"); // Markov says meow to Ned!
   
-  // bind time args are "meow" and "Kush", no call time args
-  markov.says.myBind(pavlov, "meow", "Kush")(); // Pavlov says meow to Kush!
+//   // bind time args are "meow" and "Kush", no call time args
+//   markov.says.myBind(pavlov, "meow", "Kush")(); // Pavlov says meow to Kush!
   
-  // no bind time args (other than context), call time args are "meow" and "a tree"
-  markov.says.myBind(pavlov)("meow", "a tree"); // Pavlov says meow to a tree!
+//   // no bind time args (other than context), call time args are "meow" and "a tree"
+//   markov.says.myBind(pavlov)("meow", "a tree"); // Pavlov says meow to a tree!
   
-  // bind time arg is "meow", call time arg is "Markov"
-  markov.says.myBind(pavlov, "meow")("Markov"); // Pavlov says meow to Markov!
+//   // bind time arg is "meow", call time arg is "Markov"
+//   markov.says.myBind(pavlov, "meow")("Markov"); // Pavlov says meow to Markov!
   
-  // no bind time args (other than context), call time args are "meow" and "me"
-  const notMarkovSays = markov.says.myBind(pavlov);
-  notMarkovSays("meow", "me"); // Pavlov says meow to me!
+//   // no bind time args (other than context), call time args are "meow" and "me"
+//   const notMarkovSays = markov.says.myBind(pavlov);
+//   notMarkovSays("meow", "me"); // Pavlov says meow to me!
 
 
-//2. write a second version using the ... rest operator.
+// //2. write a second version using the ... rest operator.
+
+// // Function.prototype.myBind = function (ctx, ...bindArgs) {
+// //     return(...callArgs) => this.apply(ctx, bindArgs.concat(callArgs)); 
+
+// // }
+
+
 
 
 //------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -116,8 +123,6 @@ class Cat {
 
 
 
-const sum = curriedSum(4);
-sum(5)(30)(20)(1); // => 56
 
 
 
@@ -128,9 +133,25 @@ sum(5)(30)(20)(1); // => 56
 // Function.prototype.curry
 // 1. Write a version that uses Function.prototype.apply 
 
+function curriedSum(numArgs){
+    const numbers = [];
+
+    function _curriedSum(num) {
+        numbers.push(num);
+        if (numbers.length === numArgs) {
+            let total = 0;
+            numbers.forEach((n) => {total += n })
+            return total;
+        }else{
+            return _curriedSum;
+        }
+    }
+    return _curriedSum;
+}
 
 
-
+const sum = curriedSum(4);
+console.log(sum(5)(30)(20)(1)); // => 56
 
 
 //2. Write a version that uses ... (the spread operator).

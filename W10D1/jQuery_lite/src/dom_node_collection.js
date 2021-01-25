eaeb400eaa24980e2266bf3a80d91bd25aa3d19f
Michelle Ha <michelle1992us@yahoo.com>
2021-01-25 12:18:02 -0800

    methods

--- W10D1/jQuery_lite/src/dom_node_collection.js
+++ W10D1/jQuery_lite/src/dom_node_collection.js
@@ -14,11 +14,38 @@ class DomNodeCollection {
     }
 
     empty() {
-        this.html("")
+        this.html("");
     }
 
     append(arg) {
+        if (arg instanceof DomNodeCollection) {
+            this.elements.forEach(currentNode => {
+                arg.elements.forEach(newNode => {
+                    currentNode.innerHTML = newNode.outerHTML;
+                })
+            })
+        } else if (arg instanceof HTMLElement) {
+            this.elements.forEach(currentNode => currentNode.innerHTML = arg.outerHTML);
+        } else if (typeof arg === 'string') {
+            this.elements.forEach(currentNode => currentNode.innerHTML = arg);
+        }
+    }
 
+    attr(name, value = null) {
+        if (value) {
+            this.elements.forEach(node => node.setAttribute(name, value))
+        } else {
+            this.elements.forEach(node => console.log(node.getAttribute(name)))
+            
+        }
+    }
+
+    addClass(name) {
+        this.elements.forEach(node => node.classList.add(name));
+    }
+
+    removeClass(name) {
+        this.elements.forEach(node => node.classList.remove(name));
     }
 }
 

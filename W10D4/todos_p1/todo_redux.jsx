8223aee47767595430ad7f0de0d72ed4edd71fed
Michelle Ha <michelle1992us@yahoo.com>
2021-01-28 15:42:24 -0800

    uploaded classwork

---
+++ W10D4/todos_p1/todo_redux.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import configureStore from './frontend/store/store';
+import { receiveTodo, receiveTodos, removeTodo} from './frontend/actions/todo_actions'
+import { receiveStep, receiveSteps, removeStep} from './frontend/actions/step_actions'
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    const root = document.getElementById("content");
+    ReactDOM.render(<h1>Todos App</h1>, root )
+
+    window.store = configureStore()
+    window.receiveTodo = receiveTodo
+    window.receiveTodos = receiveTodos
+    window.removeTodo = removeTodo
+    window.receiveStep = receiveStep
+    window.receiveSteps = receiveSteps
+    window.removeStep = removeStep
+
+})

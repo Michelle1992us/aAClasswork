938d4776b9d548a4b6d8b94442d8d07dd2bbeee7
Michelle Ha <michelle1992us@yahoo.com>
2021-02-03 15:40:49 -0800

    started pokemon detail container

---
+++ W11D3/pokedex-part-ii/frontend/components/pokemon/pokemon_detail_container.js
@@ -0,0 +1,18 @@
+import {connect} from 'react-redux';
+import PokemonDetail from './pokemon_detail';
+import {requestSinglePokemon} from './../../actions/pokemon_actions';
+
+
+const mapStateToProps = (state, ownProps) => {
+    return { 
+        pokemon: state.entities.pokemon[ownProps.match.params.pokemonId]
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return{
+      requestSinglePokemon: (pokemonId) => dispatch(requestSinglePokemon(pokemonId)) 
+    }
+  }
+
+export default connect(mapStateToProps, mapDispatchToProps)(PokemonDetail)

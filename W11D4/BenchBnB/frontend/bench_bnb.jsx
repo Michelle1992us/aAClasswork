58636d611a563340e74d429d24f2915594fba573
Michelle Ha <michelle1992us@yahoo.com>
2021-02-04 23:44:06 -0800

    finished phase 1

---
+++ W11D4/BenchBnB/frontend/bench_bnb.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+// import { signup, login, logout} from './util/session_api_util'
+import configureStore from './store/store';
+
+document.addEventListener("DOMContentLoaded", () => {
+  const root = document.getElementById("root");
+  const store = configureStore();
+
+  ReactDOM.render(<h1>Welcome to BenchBnB</h1>, root);
+  // window.signup = signup
+  // window.login = login
+  // window.logout = logout 
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
+
+});
